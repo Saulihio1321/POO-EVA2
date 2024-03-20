08a8f31e41726a8009fe24aa22c3fa4090c3bd60
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2024-03-19 18:47:21 -0600

    Add files via upload

---
+++ EVA2_10_interfaces/src/eva2_10_interfaces/Datos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_10_interfaces;
+
+/**
+ *
+ * @author invitado
+ */
+public interface Datos {
+    //final public int valor = 100;
+   //soloacepta constantes
+    public void imprimirDatos();
+    public String generarNombreCom();    
+    
+
+    
+    
+}
