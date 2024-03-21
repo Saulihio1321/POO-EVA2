bee234213932f90d54e3db7a812e28d224053bd1
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2024-03-20 18:47:06 -0600

    Add files via upload

---
+++ EVA2_11_has_a/src/eva2_11_has_a/EVA2_11_has_a.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_11_has_a;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_11_has_a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Direccion direc = new Direccion("5ta", "55", "valles", "cdmx", "cdmx");
+        Persona perso = new Persona("juan", "perez", 50, direc);
+        System.out.println(perso);
+       
+    }
+    
+}
