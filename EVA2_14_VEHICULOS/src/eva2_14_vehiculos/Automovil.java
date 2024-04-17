b039685fb2ba97afdecde821242c0979f6315fac
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2024-04-16 18:57:36 -0600

    Add files via upload

---
+++ EVA2_14_VEHICULOS/src/eva2_14_vehiculos/Automovil.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_14_vehiculos;
+
+/**
+ *
+ * @author invitado
+ */
+public class Automovil extends Vehiculo{
+    private int noPuertas;
+
+    public Automovil() {
+        super();
+    }
+
+    public Automovil(int noPuertas, int velocidad) {
+        super(velocidad);
+        this.noPuertas = noPuertas;
+    }
+
+    public int getNoPuertas() {
+        return noPuertas;
+    }
+
+    public void setNoPuertas(int noPuretas) {
+        this.noPuertas = noPuretas;
+    }
+
+   
+    
+     
+    
+}
